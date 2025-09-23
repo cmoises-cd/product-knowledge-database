72afa49b6d05d871ee60e4095e011c2436e51cec
Christian <c.moises@cd-commerce.de>
2025-09-23 22:52:54 +0800

    Including Routers

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from database import SessionLocal, engine, Base
 from models import productsModel
-from routers import product_router
-from routers import product_addon_router
+from routers import product_router,product_attachment_router ,product_addon_router, product_department_router
+
 # Connect to the database
 def connect_to_db():
     try:
@@ -15,10 +15,15 @@ def connect_to_db():
 # Create a FastAPI "instance"
 app = FastAPI(on_startup=[connect_to_db])
 
-# Include the product router
+# Include the product routers
 app.include_router(product_router.router)
 app.include_router(product_addon_router.router)
+app.include_router(product_attachment_router.router)
+app.include_router(product_department_router.router)
+
 # Define a path operation decorator for the root path ("/")
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Message": "Welcome to the Product information Management System API!",
+            "Version": "1.0.0",
+            "Description": "This API allows you to manage products, their addons, and attachments."}
